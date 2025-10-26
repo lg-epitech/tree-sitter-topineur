6fdcc097335ab7f4878812adb4b4a7850236940d
Laurent Gonzalez <laurent.gonzalez@epitech.eu>
2025-10-27 00:13:34 +0100

    feat: object type parsing

--- grammar.js
+++ grammar.js
@@ -13,13 +13,18 @@ module.exports = grammar({
   extras: $ => [/\s/, $.comment],
 
   rules: {
-    // TODO: add the actual grammar rules
     source_file: $ => repeat($.__top_level),
 
     comment: () => token(seq("|-", /.*/)),
 
     __top_level: $ =>
-      choice($.package_decl, $.import_decl, $.function_definition),
+      choice(
+        $.package_decl,
+        $.import_decl,
+        $.function_definition,
+        $.object_type_declaration,
+        $.let_binding
+      ),
 
     package_decl: ($) => seq("package", field("name", $.identifier)),
 
@@ -31,53 +36,222 @@ module.exports = grammar({
         "def",
         field("name", $.identifier),
         field("params", $.parameter_list),
-        optional(seq(":", field("return_type", $.type_identifier))),
+        optional(seq(":", field("return_type", $.type))),
         field("body", $.block)
+      ),
+
+    object_type_declaration: ($) =>
+      seq(
+        "object",
+        "type",
+        field("name", $.identifier),
+        "{",
+        repeat(choice($.field_declaration, $.function_definition)),
+        "}"
+      ),
+
+    field_declaration: ($) =>
+      seq(
+        field("name", $.identifier),
+        ":",
+        field("type", $.type),
+        optional(seq("=", field("default", $._expression)))
+      ),
+
+    let_binding: ($) =>
+      seq(
+        "let",
+        field("name", choice($.identifier, $.tuple_pattern)),
+        optional(seq(":", field("type", $.type))),
+        "=",
+        field("value", $._expression)
+      ),
+
+    tuple_pattern: ($) =>
+      seq(
+        "(",
+        $.identifier,
+        ",",
+        seq($.identifier, repeat(seq(",", $.identifier))),
+        ")"
       ),
 
     parameter_list: ($) =>
       seq(
         "(",
-        optional(seq($.parameter, repeat(seq(",", $.identifier)))),
+        optional($._parameter_sequence),
         ")"
       ),
+
+    _parameter_sequence: ($) =>
+      seq($.parameter_or_tuple, repeat(seq(",", $.parameter_or_tuple))),
+
+    parameter_or_tuple: ($) =>
+      choice($.parameter, $.tuple_pattern),
 
     parameter: ($) =>
       seq(
         field("name", $.identifier),
         ":",
-        field("type", $.type_identifier)
+        field("type", $.type)
       ),
 
     block: ($) => seq("{", repeat($._statement), "}"),
 
     _statement: ($) =>
-      choice($.top_statement, $.if_expression, $.expression),
+      choice($.top_statement, $.if_expression, $.let_binding, $._expression),
 
     _simple_statement: ($) =>
-      choice($.top_statement, $.expression),
+      choice($.top_statement, $.if_expression, $.let_binding, $._expression),
 
     if_expression: ($) =>
-      seq(
-        "if",
-        field("condition", $.expression),
-        "then",
-        field("then_branch", $._simple_statement),
-        optional(seq(
-        "else",
-        field("else_branch", $._simple_statement)))
+      prec.left(1,
+        seq(
+          "if",
+          field("condition", $._expression),
+          "then",
+          field("then_branch", $._simple_statement),
+          optional(seq("else", field("else_branch", $._simple_statement)))
+        )
+      ),
+
+    _expression: ($) =>
+      prec.right(2, choice(
+        $.let_expression,
+        $.binary_expression,
+        $._call_or_member_expression
+      )),
+
+    let_expression: ($) =>
+      prec.right(3,
+        seq(
+          "let",
+          field("name", $.identifier),
+          "=",
+          field("value", $._expression),
+          "in",
+          field("body", $._expression)
+        )
+      ),
+
+    _call_or_member_expression: ($) =>
+      choice(
+        $.method_call,
+        $.call_expression,
+        $.member_expression,
+        $._primary_expression
       ),
 
     _primary_expression: ($) =>
-      choice($.identifier, $.number, $.string, $.call_expression),
+      choice(
+        $.identifier,
+        $.self_reference,
+        $.number,
+        $.float_number,
+        $.string,
+        $.tuple_expression,
+        $.array_expression,
+        $.object_literal,
+        $.parenthesized_expression,
+        prec(1, $.anonymous_function)
+      ),
+
+    parenthesized_expression: ($) => seq("(", $._expression, ")"),
+
+    member_expression: ($) =>
+      prec.left(1,
+        seq(field("object", $._primary_expression), ".", field("member", $.identifier))
+      ),
 
     call_expression: ($) =>
+      prec.left(1,
+        seq(
+          field("function", $._primary_expression),
+          "(",
+          optional($._argument_sequence),
+          ")"
+        )
+      ),
+
+    _argument_sequence: ($) =>
+      seq($._expression, repeat(seq(",", $._expression))),
+
+    method_call: ($) =>
+      prec.left(2,
+        seq(
+          field("object", $._primary_expression),
+          ".",
+          field("method", $.identifier),
+          "(",
+          optional($._argument_sequence),
+          ")"
+        )
+      ),
+
+    self_reference: () => "self",
+
+    object_literal: ($) =>
       seq(
-        field("function", $.identifier),
+        field("type", $.identifier),
+        "{",
+        optional($._field_assignment_sequence),
+        "}"
+      ),
+
+    _field_assignment_sequence: ($) =>
+      seq($.field_assignment, repeat(seq(",", $.field_assignment))),
+
+    field_assignment: ($) =>
+      seq(
+        field("name", $.member_or_identifier),
+        "=",
+        field("value", $._expression)
+      ),
+
+    member_or_identifier: ($) =>
+      choice($.member_expression, $.identifier),
+
+    array_expression: ($) =>
+      seq(
+        "[",
+        optional($._array_elements),
+        "]"
+      ),
+
+    _array_elements: ($) =>
+      seq($._expression, repeat(seq(",", $._expression))),
+
+    tuple_expression: ($) =>
+      seq(
         "(",
-        optional(seq($.expression, repeat(seq(",", $.expression)))),
+        $._expression,
+        ",",
+        seq($._expression, repeat(seq(",", $._expression))),
         ")"
       ),
+
+    anonymous_function: ($) =>
+      prec.left(10,
+        seq(
+          "fun",
+          $.untyped_parameter_list,
+          "->",
+          field("body", $._expression)
+        )
+      ),
+
+    untyped_parameter_list: ($) =>
+      seq(
+        "(",
+        optional($._untyped_parameter_sequence),
+        ")"
+      ),
+
+    _untyped_parameter_sequence: ($) =>
+      seq($.identifier_or_tuple, repeat(seq(",", $.identifier_or_tuple))),
+
+    identifier_or_tuple: ($) =>
+      choice($.identifier, $.tuple_pattern),
 
     expression: ($) =>
       choice($.binary_expression, $._primary_expression),
@@ -86,21 +260,37 @@ module.exports = grammar({
 
     binary_expression: ($) =>
       prec.left(
-        1,
         seq(
-          field("left", $.expression),
+          field("left", $._expression),
           field("operator", $.operators),
-          field("right", $.expression)
+          field("right", $._expression)
         )
       ),
 
-    top_statement: ($) => seq("top", $.expression),
+    top_statement: ($) => seq("top", $._expression),
+
+    type: ($) =>
+      choice($.generic_type, $.type_identifier, $.identifier),
+
+    generic_type: ($) =>
+      seq(
+        field("name", $.identifier),
+        "[",
+        $._type_spec,
+        repeat(seq(",", $._type_spec)),
+        "]"
+      ),
+
+    _type_spec: ($) =>
+      choice($.generic_type, $.type_identifier, $.identifier),
 
     identifier: () => /[a-zA-Z_]\w*/,
 
     type_identifier: () => /[A-Z]\w*/,
 
     number: () => /\d+/,
+
+    float_number: () => /\d+\.\d+/,
 
     string: () => seq('"', /[^"]*/, '"'),
   }
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -13,6 +13,11 @@
   "top"
   "package"
   "import"
+  "object"
+  "type"
+  "let"
+  "in"
+  "fun"
 ] @keyword
 
 ;; Operators
@@ -27,8 +32,10 @@
 ;; Punctuation
 ;; ------------------------------
 [
-  "(" ")" "{" "}"
-  ":"
+  "(" ")" "{" "}" "[" "]"
+  ":" "."
+  ","
+  "=" "->"
 ] @punctuation.bracket
 
 [
@@ -36,17 +43,53 @@
 ] @constant.builtin
 
 ;; ------------------------------
-;; General identifiers (fallback - must be FIRST)
+;; General identifiers (fallback)
 ;; ------------------------------
 (identifier) @variable
+
+;; ------------------------------
+;; Object types
+;; ------------------------------
+(object_type_declaration name: (identifier) @type)
+(field_declaration name: (identifier) @variable.other.member)
+(field_declaration type: (type) @type)
+
+;; ------------------------------
+;; Let bindings
+;; ------------------------------
+(let_binding name: (identifier) @variable)
+(let_binding type: (type) @type)
+(let_expression name: (identifier) @variable)
+
+;; ------------------------------
+;; Self references
+;; ------------------------------
+(self_reference) @variable.builtin
+
+;; ------------------------------
+;; Member access
+;; ------------------------------
+(member_expression member: (identifier) @variable.other.member)
+
+;; ------------------------------
+;; Method calls
+;; ------------------------------
+(method_call method: (identifier) @function.method)
+
+;; ------------------------------
+;; Object literals
+;; ------------------------------
+(object_literal type: (identifier) @type)
+(field_assignment) @variable.other.member
 
 ;; ------------------------------
 ;; Function definitions (override variable for function names)
 ;; ------------------------------
 (function_definition name: (identifier) @function)
 (function_definition decorator: (identifier) @constant.builtin)
-(function_definition return_type: (type_identifier) @type)
+(function_definition return_type: (type) @type)
 (parameter name: (identifier) @variable.parameter)
+(parameter type: (type) @type)
 
 ;; ------------------------------
 ;; Function calls (override variable for function names)
@@ -57,6 +100,9 @@
 (call_expression function: (identifier) @function.builtin
   (#match? @function.builtin "^(println|show)$"))
 
+;; Anonymous functions
+(anonymous_function) @function
+
 ;; ------------------------------
 ;; Declarations
 ;; ------------------------------
@@ -64,8 +110,24 @@
 (import_decl name: (identifier) @namespace)
 
 ;; ------------------------------
+;; Type identifiers and their contents
+;; ------------------------------
+(type_identifier) @type
+
+;; Generic types - highlight name and type parameters
+(generic_type name: (identifier) @type)
+(generic_type) @type
+
+;; Recursively highlight types inside types
+(type (type_identifier) @type)
+(type (identifier) @type)
+(type (generic_type) @type)
+
+;; ------------------------------
 ;; Literals
 ;; ------------------------------
 (string) @string
 (number) @number
-(type_identifier) @type
+(float_number) @number
+(tuple_expression) @constructor
+(array_expression) @constructor
